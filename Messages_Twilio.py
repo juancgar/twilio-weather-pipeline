f548a27f43744aca02075e103b1c8b9f32dfb812
Juan Carlos Garza Lopez <juangarza373@gmail.com>
2023-08-09 16:20:10 -0600

    added the new python scripts

---
+++ Messages_Twilio.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import os
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+from datetime import datetime
+from twilio.rest import Client
+import time
+from dotenv import load_dotenv
+from utils import request_wapi,get_forecast,create_df,send_message,get_date
+
+
+query = 'Tokyo'
+load_dotenv(dotenv_path="Config.env")
+
+twilio_acc = os.getenv('TWILIO_ACC')
+twilio_secret = os.getenv('TWILIO_SECRET')
+WEATHER_API = os.getenv('WEATHER_API')
+
+
+input_date = get_date()
+response = request_wapi(WEATHER_API,query)
+
+datos = []
+
+for i in tqdm(range(24),colour = 'green'):
+
+    datos.append(get_forecast(response,i))
+
+
+df_rain = create_df(datos)
+
+# Send Message
+message_id = send_message(twilio_acc,twilio_secret,input_date,df_rain,query)
+
+
+print('Mensaje Enviado con exito ' + message_id)
+
+
+
+
+
